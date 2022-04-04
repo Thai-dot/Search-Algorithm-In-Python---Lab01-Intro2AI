5400d09af1d41f3faf90c210dc10aae13303319f
Thai-dot <nguyenhoangthai7871@gmail.com>
2022-04-04 16:13:37 +0700

    final commit

--- student_functions.py
+++ student_functions.py
@@ -1,8 +1,15 @@
 import numpy as np
-from queue import PriorityQueue
-
 from sympy import false
 
+# ------------------References------------------: 
+# https://www.redblobgames.com/pathfinding/a-star/implementation.html
+# https://python.plainenglish.io/uniform-cost-search-ucs-algorithm-in-python-ec3ee03fca9f
+# https://stackoverflow.com/questions/8922060/how-to-trace-the-path-in-a-breadth-first-search
+# https://www.youtube.com/watch?v=pcKY4hjDrxk&t=41s
+# https://www.youtube.com/watch?v=dRMvK76xQJI&t=305s
+# https://www.youtube.com/watch?v=6TsL96NAZCo
+# https://www.youtube.com/watch?v=dv1m3L6QXWs&t=2s
+# https://en.wikipedia.org/wiki/A*_search_algorithm
 
 def BFS(matrix, start, end):
 
@@ -73,25 +80,33 @@ def UCS(matrix, start, end):
     frontier.update({start: (0, None)})
 
     while frontier:
+        #get the node with the lowest cost
         node = min(frontier.items(), key=lambda x: x[1][0])[0]
+        #mark the node as visited
         visited.update({node: frontier[node][1]})
 
+        #if the node is the end node, break the node
         if node == end:
             break
-
+        #for each adjacent node
         for i in range(len(matrix[node])):
+            #if the adjacent node is not visited
             if matrix[node][i] > 0 and i not in visited:
+                #get the weight of the adjacent node plus the previous node cost
                 new_cost = frontier.get(node)[0] + matrix[node][i]
+                #check if the adjacent node is not in the frontier or if the new cost is lower than the old cost
                 if i not in frontier or new_cost < frontier.get(i)[0]:
                     frontier.update({i: (new_cost, node)})
 
         del frontier[node]  # delete the node from the frontier
 
+    # backtrack to get the path
     node = end
     while node != start:
         path.append(node)
         node = visited[node]
     path.append(start)
+    # reverse the path
     path = path[::-1]
 
     return visited, path
@@ -103,56 +118,92 @@ def GBFS(matrix, start, end):
     path = []
     visited = {}
 
+    #create closedList and openList
     closedList = []
     openList = {}
+    #push the first node into the openList
     openList.update({start: (0, None)})
 
     while openList:
+        #get the node with the lowest cost
         node = min(openList.items(), key=lambda x: x[1][0])[0]
+        #add the node to the closedList
         closedList.append(node)
+        #mark the node as visited
         visited.update({node: openList[node][1]})
+        #if the node is the end node, break the node
         if node == end:
             break
+        #for each adjacent node
         for i in range(len(matrix[node])):
+            #if the adjacent node is not visited
             if matrix[node][i] > 0 and i not in closedList:
+                #get the weight of the adjacent node
                 edge_weight = matrix[node][i]
+                #check if the adjacent node is not in the openList or if the new cost is lower than the old cost
                 if i not in openList or edge_weight < openList.get(i)[0]:
                     openList.update({i: (edge_weight, node)})
 
         del openList[node]
 
+    #backtrack to get the path
     node = end
     while node != start:
         path.append(node)
         node = visited[node]
     path.append(start)
+    #reverse the path
     path = path[::-1]
 
     return visited, path
 
+#This function is a euclidean dist
+#heuristic which is used to calculate the euclidean distance between two points
+def euclidean_distance(x1, y1, x2, y2):
+    return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
 
 def Astar(matrix, start, end, pos):
-    """
-    A* Search algorithm
-     Parameters:
-    ---------------------------
-    matrix: np array UCS
-        The graph's adjacency matrix
-    start: integer 
-        starting node
-    end: integer
-        ending node
-    pos: dictionary. keys are nodes, values are positions
-        positions of graph nodes
-    Returns
-    ---------------------
-    visited
-        The dictionary contains visited nodes: each key is a visited node, 
-        each value is the key's adjacent node which is visited before key.
-    path: list
-        Founded path
-    """
     # TODO:
     path = []
     visited = {}
+    
+    frontier = {}
+    #Fn = g(n) + h(n), the first g(n) is zero, 
+    #the second h(n) is the euclidean distance between the current node and the end node
+    Fn = euclidean_distance(pos[end][0], pos[end]
+                            [1], pos[start][0], pos[start][1])
+    # push the first node into the frontier
+    frontier.update({start: (0, None, Fn)})
+
+    while frontier:
+        #get the node with the lowest f(n)
+        node = min(frontier.items(), key=lambda x: x[1][2])[0]
+        #mark the node as visited
+        visited.update({node: frontier[node][1]})
+        #if the node is the end node, break the node
+        if node == end:
+            break
+        #for each adjacent node
+        for i in range(len(matrix[node])):
+            #if the adjacent node is not visited
+            if matrix[node][i] > 0 and i not in visited:
+                #calculate the F(n) = g(n) + h(n) and new_cost
+                new_Fn = frontier.get(node)[0] + matrix[node][i] + euclidean_distance(
+                    pos[end][0], pos[end][1], pos[i][0], pos[i][1])
+                new_cost = frontier.get(node)[0] + matrix[node][i]
+                #if the adjacent node is not in the frontier or the new_Fn is smaller than the old Fn
+                if i not in frontier or new_Fn < frontier.get(i)[2]:
+                    frontier.update({i: (new_cost, node,new_Fn)}) #update the frontier
+
+        del frontier[node]  # delete the node from the frontier after visit it
+
+    #backtrack the path
+    node = end
+    while node != start:
+        path.append(node)
+        node = visited[node]
+    path.append(start)
+    #reverse the path
+    path = path[::-1]
     return visited, path
